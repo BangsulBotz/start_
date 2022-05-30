245fcf0b1b914a974314fa4f4c5a16cd8437ea3c
BangsulBotz <99018264+BangsulBotz@users.noreply.github.com>
2022-05-30 18:22:05 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+        "clientID": "R03rjS4Jj+dP+sk6Mx6K0w==",
+        "serverToken": "1@Noc0tHWH4KvHWWE7acDWluS0vHQKxHHAT0QdcpD8V+95Fk4JdLACzUaX5AIBtQhPN3YMosywKizdlQ==",
+        "clientToken": "xBcxqtYjV0v0y76HY0xsnI56uAT1OiZlCGFDA48HTGs=",
+        "encKey": "65tg/tj0/SuK/fvZm3gH6pVzMNNFRl98emHdfZEiI1k=",
+        "macKey": "b2Eig2zVVSeeVFiR7C5G2EAQiFwL5f6KIOVutRj7JGY="
+}
